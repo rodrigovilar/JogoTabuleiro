0ee1b9bc0559188bf5a706bf525fc85d4c9ea725
jonathasfirmo <jonathas.anderson@dce.ufpb.br>
2013-05-24 00:02:05 -0300

    Criação de alguns metodos inicias de teste.

--- src/ufpb/br/projeto/JogoTest.java
+++ src/ufpb/br/projeto/JogoTest.java
@@ -2,13 +2,53 @@ package ufpb.br.projeto;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class JogoTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	private Jogo jogo;
+
+	@Before
+	public void criarJogo() {
+		jogo = new Jogo();
 	}
+
+	@Test
+	public void iniciarJogo() {
+		assertFalse("O jogo iniciou acabado", jogo.acabou());
+	}
+
+	@Test
+	public void definirPersonagemX() {
+		jogo.setPersonagemX(true);
+		assertTrue("Esperava que o primeiro personagem fosse X",
+				jogo.isPersonagemX());
+
+	}
+
+	@Test
+	public void definirPersonagemXDeNovo() {
+		jogo.setPersonagemX(true);
+		jogo.setPersonagemX(false);
+		assertFalse("esperava que o primeiro personagem fosse Y",
+				jogo.isPersonagemX());
+	}
+
+	@Test
+	public void definirPersonagemXAposInicio() {
+		// TODO
+		// Nao da para fazer esse teste, pois nao sabemos como ele inicializa.
+	}
+
+	@Test
+	public void jogarDado() {
+		jogo.setPersonagemX(true);
+		jogo.lancarDado();
+		assertTrue("espera-se um numero > 0 e < 7",
+				jogo.getDado() > 0 && jogo.getDado() < 7);
+
+	}
+	
 
 }
