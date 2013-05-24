0ef713bb460bf8b56c0c80e9f79dbde6056d7f22
jonathasfirmo <jonathas.anderson@dce.ufpb.br>
2013-05-24 00:02:38 -0300

    Comentando a classe Personagem

--- src/ufpb/br/projeto/Personagem.java
+++ src/ufpb/br/projeto/Personagem.java
@@ -1,5 +1,13 @@
 package ufpb.br.projeto;
 
+/**
+ * Essa classe sera composta por dois personagem: X e Y
+ * que competiram no jogo.
+ * 
+ * @author jonathas Firmo
+ *
+ */
+
 public class Personagem {
 
 }
