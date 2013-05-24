0076f51db67ebacca81e445639c6fb50aca99d4f
jonathasfirmo <jonathas.anderson@dce.ufpb.br>
2013-05-24 00:03:34 -0300

    Criação de alguns métodos iniciais para fazer os testes passarem

--- src/ufpb/br/projeto/Jogo.java
+++ src/ufpb/br/projeto/Jogo.java
@@ -1,5 +1,33 @@
 package ufpb.br.projeto;
 
+/**
+ * Essa classe agregarar inderetamente todas as outras entidades
+ * 
+ * @author jonathas Firmo
+ * 
+ */
 public class Jogo {
+
+	private boolean definirPersonagemX;
+
+	public boolean acabou() {
+		return false;
+	}
+
+	public boolean isPersonagemX() {
+		return definirPersonagemX;
+	}
+
+	public void setPersonagemX(boolean b) {
+		this.definirPersonagemX = b;
+	}
+
+	public void lancarDado() {
+
+	}
+
+	public int getDado() {
+		return 1;
+	}
 
 }
