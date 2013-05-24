96942fd7ed7452e69cdefe8a589979ad19ce50c8
AdrianoPatricio <adryanopatricio@hotmail.com>
2013-05-23 21:03:17 -0300

    Criação da classe JogoTest

---
+++ src/ufpb/br/projeto/JogoTest.java
@@ -0,0 +1,14 @@
+package ufpb.br.projeto;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JogoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
