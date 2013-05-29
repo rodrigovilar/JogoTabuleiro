460313f3c2c692ba26c6325df052d1edecfe768a
AdrianoPatricio <adryanopatricio@hotmail.com>
2013-05-29 00:53:00 -0300

    Criação da classe JogoFacadeTest

---
+++ src/ufpb/br/projeto/JogoFacadeTest.java
@@ -0,0 +1,14 @@
+package ufpb.br.projeto;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JogoFacadeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
