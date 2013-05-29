b556c419df5946243a9a0157d2744e17dd324bff
AdrianoPatricio <adryanopatricio@hotmail.com>
2013-05-29 00:57:03 -0300

    Remoção dos metodos da classe JogoTest para JogoFacadeTest

--- src/ufpb/br/projeto/JogoFacadeTest.java
+++ src/ufpb/br/projeto/JogoFacadeTest.java
@@ -2,13 +2,60 @@ package ufpb.br.projeto;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class JogoFacadeTest {
 
+private Jogo jogo;
+	
+	//Teste
+
+	@Before
+	public void criarJogo() {
+		jogo = new Jogo();
+	}
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void iniciarJogo() {
+		assertFalse("O jogo iniciou acabado", jogo.acabou());
+	}
+
+	@Test
+	public void definirPersonagemX() {
+		jogo.setPersonagemX(true);
+		assertTrue("Esperava que o primeiro personagem fosse X",
+				jogo.isPersonagemX());
+
+	}
+
+	@Test
+	public void definirPersonagemXDeNovo() {
+		jogo.setPersonagemX(true);
+		jogo.setPersonagemX(false);
+		assertFalse("esperava que o primeiro personagem fosse Y",
+				jogo.isPersonagemX());
+	}
+
+	@Test
+	public void definirPersonagemXAposInicio() {
+		// TODO
+		// Nao da para fazer esse teste, pois nao sabemos como ele inicializa.
+	}
+
+	@Test
+	public void scoreZero(){
+		// Testa se o score inicializou zerado
+		jogo.setPersonagemX(true);
+		assertEquals(jogo.getScore(),0);
+	}
+	
+	@Test
+	public void jogarDado() {
+		jogo.setPersonagemX(true);
+		assertTrue("espera-se um numero > 0 e < 7",
+				jogo.lancarDado() > 0 && jogo.lancarDado() < 7);
+
 	}
 
 }
--- src/ufpb/br/projeto/JogoTest.java
+++ src/ufpb/br/projeto/JogoTest.java
@@ -5,58 +5,8 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+@SuppressWarnings("unused")
 public class JogoTest {
-
-	private Jogo jogo;
 	
-	//Teste
-
-	@Before
-	public void criarJogo() {
-		jogo = new Jogo();
-	}
-
-	@Test
-	public void iniciarJogo() {
-		assertFalse("O jogo iniciou acabado", jogo.acabou());
-	}
-
-	@Test
-	public void definirPersonagemX() {
-		jogo.setPersonagemX(true);
-		assertTrue("Esperava que o primeiro personagem fosse X",
-				jogo.isPersonagemX());
-
-	}
-
-	@Test
-	public void definirPersonagemXDeNovo() {
-		jogo.setPersonagemX(true);
-		jogo.setPersonagemX(false);
-		assertFalse("esperava que o primeiro personagem fosse Y",
-				jogo.isPersonagemX());
-	}
-
-	@Test
-	public void definirPersonagemXAposInicio() {
-		// TODO
-		// Nao da para fazer esse teste, pois nao sabemos como ele inicializa.
-	}
-
-	@Test
-	public void scoreZero(){
-		// Testa se o score inicializou zerado
-		jogo.setPersonagemX(true);
-		assertEquals(jogo.getScore(),0);
-	}
-	
-	@Test
-	public void jogarDado() {
-		jogo.setPersonagemX(true);
-		assertTrue("espera-se um numero > 0 e < 7",
-				jogo.lancarDado() > 0 && jogo.lancarDado() < 7);
-
-	}
-	
-	
+	//Remoção do atributo e metodos para a classe JogoFacadeTest
 }
