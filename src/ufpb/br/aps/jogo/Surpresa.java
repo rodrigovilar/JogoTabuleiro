a4d4b100e91d5d2e2ba0bb68a6109220a096343e
AdrianoPatricio <adryanopatricio@hotmail.com>
2013-06-10 03:33:12 -0300

    Criação do atributo valorNumerico na classe Surpresa

--- src/ufpb/br/aps/jogo/Surpresa.java
+++ src/ufpb/br/aps/jogo/Surpresa.java
@@ -1,11 +1,7 @@
 package ufpb.br.aps.jogo;
 
-/**
- * Essa classe terá uma surpresa para o personagem
- * 
- * @author jonathas firmo
- *
- */
 public class Surpresa {
-
+	
+	private int valorNumerico;
+	
 }
