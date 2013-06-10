d51f20c258e02950b65b7b23c6240dfe09ba9a62
AdrianoPatricio <adryanopatricio@hotmail.com>
2013-06-10 03:43:29 -0300

    Criação do atributo nome na classe Personagem

--- src/ufpb/br/aps/jogo/Personagem.java
+++ src/ufpb/br/aps/jogo/Personagem.java
@@ -1,13 +1,6 @@
 package ufpb.br.aps.jogo;
 
-/**
- * Essa classe sera composta por dois personagem: X e Y
- * que competiram no jogo.
- * 
- * @author jonathas Firmo
- *
- */
-
 public class Personagem {
-
+	
+	private String nome;
 }
