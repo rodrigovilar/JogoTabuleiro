56f73e987091dc85b31543df860fbb0b3d7549b0
AdrianoPatricio <adryanopatricio@hotmail.com>
2013-06-11 10:44:12 -0300

    Criação da lista personagens na classe GerentePersonagem

--- src/br/ufpb/aps/controle/GerentePersonagem.java
+++ src/br/ufpb/aps/controle/GerentePersonagem.java
@@ -1,5 +1,11 @@
 package br.ufpb.aps.controle;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ufpb.br.aps.jogo.entidade.Personagem;
+
 public class GerentePersonagem {
 
+	private List<Personagem> personagens = new ArrayList<Personagem>();
 }
