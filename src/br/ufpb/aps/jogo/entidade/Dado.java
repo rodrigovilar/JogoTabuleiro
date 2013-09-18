65cc0ee48707733464f305135f0975997aa7f29a
jonathasfirmo <jonathas.anderson@dce.ufpb.br>
2013-09-18 10:59:07 -0300

    Criando a estrutura lógica do Dado.

--- src/br/ufpb/aps/jogo/entidade/Dado.java
+++ src/br/ufpb/aps/jogo/entidade/Dado.java
@@ -1,14 +1,39 @@
 package br.ufpb.aps.jogo.entidade;
 
+import java.util.Random;
+/**
+ * 
+ * Essa Classe funcionará como um Dado,ela retornará um valor aleatorio de 1 a 6
+ * atraves do metodo lancarDado.
+ * 
+ * @author jonathas
+ *
+ */
 public class Dado {
 	
+	private Random random;
 	private int valorDoDado;
-
+	private static final int FACES_DO_DADO = 2;// o numero de faces correto é 7
+	
+	public Dado(){
+		this.random = new Random();
+	}
+	/**
+	 * Esse metodo faz a acao de jogar o Dado.
+	 * @return retorna o valor que saiu no Dado.
+	 */
 	public int lancarDado(){
-		return valorDoDado = 1;
+		
+		int valorAleatorio = this.random.nextInt(FACES_DO_DADO);
+		if(valorAleatorio == 0){
+			valorAleatorio = 1;
+		}
+		valorDoDado = valorAleatorio;
+		return valorDoDado;
 	}
 	
 	public int getValorDoDado(){
 		return valorDoDado;
 	}
+	
 }
